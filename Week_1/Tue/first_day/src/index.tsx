130117c43ae423a47dc45b2a9358d2e6c29a60fc
chobobdev <chobobdev@naver.com>
2020-09-02 10:33:52 +0900

    index.tsx Updated

--- Week_1/Tue/first_day/src/index.tsx
+++ Week_1/Tue/first_day/src/index.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App(){
+/** 
+function App(props){
   return (
-    <h1> WooWa Tech Learning</h1>
+    <h1>props.title</h1>
+  )
+}
+이 방법은 Typescript에서는 불가능 한 방법이다
+*/
+
+interface AppProps {
+  title: string;
+  color: string;
+}
+//interface를 이용한 Type설정(?)
+
+function App(props: AppProps){
+  return (
+    <h1>{props.title}</h1>
   )
 }
 
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="WOOWA TECH LEARNING" color="red"/> 
   </React.StrictMode>,
   document.getElementById('root')
 );
+// 가장 기본적인 Component를 만드는 방법
